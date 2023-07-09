683235c258d020aa38bb426e99651d894b3d146d
姜明辉 <jiangmh@bibibi.net>
2023-07-09 13:17:11 +0800

    fix(*): fix load resource 404

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-demo(一个关于学习了解react的日常小demo)
 
+# [访问地址](<https://mutter45.github.io/react-demo/>)
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [react()],
   build: {
     // 在 outDir 中生成 manifest.json
