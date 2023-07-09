801e531b44032ffba2b0d8b1ba225ded3fd4ceba
姜明辉 <jiangmh@bibibi.net>
2023-07-09 12:07:04 +0800

    add(*): add deploy static sites

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 在 outDir 中生成 manifest.json
+    manifest: true,
+    // rollupOptions: {
+    //   // 覆盖默认的 .html 入口
+    //   input: '/path/to/main.js'
+    // }
+  }
 })
