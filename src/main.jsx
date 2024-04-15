1f585b0e76b2ff2ca804e5ea9ca27d88854a1f5f
Mutter <mutter45@foxmail.com>
2024-04-15 12:54:17 +0800

    feat: add router basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './main.css';
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./main.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-demo">
       <App />
     </BrowserRouter>
   </React.StrictMode>
