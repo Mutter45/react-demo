73e40b65f22c8623b565be243b41c90fa114188d
Mutter <mutter45@foxmail.com>
2024-04-15 13:13:48 +0800

    fix: delete HashRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./main.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/react-demo">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
