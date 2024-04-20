fd1fda219b29738a688945663efb52b695872f4c
Mutter <mutter45@foxmail.com>
2024-04-21 00:02:14 +0800

    feat: add husky and commitlint

---
+++ scripts/lint-staged.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  // '*.{css,scss,less}': ['stylelint --fix'],
+  // '*.{md,mdx}': ['markdownlint --fix'],
+  '*.{json,json5}': ['prettier --write'],
+  '*.{yml,yaml}': ['prettier --write'],
+}
