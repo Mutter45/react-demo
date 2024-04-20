cda5213396c5b30b2aafb8590344c817aa8b4c2d
Mutter <mutter45@foxmail.com>
2024-04-20 21:59:06 +0800

    chore: upgrade dependencies

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,8 +1,3 @@
-// eslint-disable-next-line react/prop-types
 export default function MyButton({ count, onClick }) {
-  return (
-    <>
-      <button onClick={onClick}>Clicked {count} times</button>
-    </>
-  )
+  return <button onClick={onClick}>Clicked {count} times</button>
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line react/prop-types
 export default function Square({ value, onSquareClick }) {
   return (
     <span
